cd533ec39653b2c83da9eff7e51421b8baf8932c
Wimp <potencjalny.m@gmail.com>
2019-11-12 15:49:05 +0100

    add 3d visualization tool

---
+++ 3D_cube_visualization/OpenGLCubeMesh.h
@@ -0,0 +1,32 @@
+#ifndef OPENGLCUBEMESH_H
+#define OPENGLCUBEMESH_H
+
+
+class OpenGLCubeMesh
+{
+    public:
+        int x;
+		int y;
+		int z;
+		float gap;
+
+		float* vertices;
+
+
+
+
+        OpenGLCubeMesh(int, int, int, float);
+        void saveToFile();
+        void printVertices();
+		void normalizeVertices();
+		float* getVertices();
+
+        ~OpenGLCubeMesh();
+
+
+    protected:
+
+    private:
+};
+
+#endif // OPENGLCUBEMESH_H
